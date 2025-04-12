460b25799148204f3ada3a978c237e961f45f20b
abhinavsrepository <abhi2510979b@gmail.com>
2025-04-12 23:16:18 +0530

    initial

---
+++ bariJava/Annotation.java
@@ -0,0 +1,21 @@
+class My<T>
+{
+    @SafeVarargs
+    private void show (T...arg)
+    {
+        for (T x:arg)
+        System.out.println(x);
+    }
+}
+
+
+
+
+
+
+public class Annotation {
+    public static void main(String[] args) {
+        
+    }
+    
+}
